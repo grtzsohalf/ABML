63a71102318f4175f4acb6d6390adc8c4f6eb521
grtzsohalf <b02901074@ntu.edu.tw>
2017-06-01 01:46:01 +0800

    concatenate y, p for LSTM input & predict from all labels instead of positive labels while training.

---
+++ nus-wide/train_recursive_concat.py
@@ -0,0 +1,21 @@
+from core.solver_recursive import CaptioningSolver
+from core.model_recursive import CaptionGenerator
+from core.utils_nus import *
+import os
+os.environ['CUDA_VISIBLE_DEVICES']='0'
+
+def main():
+    word_to_idx = load_word_to_idx(data_path='./nusdata', split='train')
+    model = CaptionGenerator(word_to_idx, dim_feature=[196, 1024], dim_embed=64,
+                            dim_hidden=1024, n_time_step=11, prev2out=True,
+                            ctx2out=True, alpha_c=1.0, selector=True, dropout=True)
+    data_path = './nusdata'
+    solver = CaptioningSolver(model, data_path, n_epochs=100, batch_size=128,
+                update_rule='adam', learning_rate=0.0003, print_every=30, save_every=1,
+                pretrained_model=None, model_path='model/',
+                test_model='model/model-1', print_bleu=True, log_path='log/', V=len(word_to_idx),\
+                n_time_step=11)
+    solver.train()
+
+if __name__ == "__main__":
+    main()
